88d759cb79d15cbdc87fe5540b4e2a158b68c656
Nora <norapasquale@gmail.com>
2022-09-17 18:49:35 +0200

    js

---
+++ Scripts/mainjs.js
@@ -0,0 +1,159 @@
+
+		String.prototype.tpl = function(o) { 
+			var r = this ; 
+			for (var i in o) { 
+				r = r.replace(new RegExp("\\$"+i, 'g'),o[i])  
+			} 
+			return r 
+		}
+		
+		var listItemTpl = `<li><a href='#' onclick='load("$url")'>$label</a></li>`
+		
+		$(document).ready(main);
+
+		function main() {
+			$.ajax({
+				method: 'GET',
+				url: 'https://imwtproject.github.io/howdowecallitmagazine/frank.json',
+				success: function(d) {
+					for (var i=0; i<d.length; i++) {
+						$('#list').append(listItemTpl.tpl({url:d[i].url, label: d[i].label}))
+					}	
+				},
+				error: function() {
+					alert('No document to show')
+				}
+			});
+			
+			$('#showperson').click(function() {
+				if (this.checked) 
+					$('.person').addClass('text-person')
+				else
+					$('.person').removeClass('text-person')
+			})
+			
+			$('#showcharacter').click(function() {
+				if (this.checked) 
+					$('.character').addClass('text-character')
+				else
+					$('.character').removeClass('text-character')
+			})
+			
+			$('#showliteraryWork').click(function() {
+				if (this.checked) 
+					$('.literaryWork').addClass('text-literaryWork')
+				else
+					$('.literaryWork').removeClass('text-literaryWork')
+			})
+			
+			$('#showconcept').click(function() {
+				if (this.checked) 
+					$('.concept').addClass('text-concept')
+				else
+					$('.concept').removeClass('text-concept')
+			})
+			
+			$('#showrefOther').click(function() {
+				if (this.checked) 
+					$('q.refOther').addClass('text-refOther')
+				else
+					$('q.refOther').removeClass('text-refOther')
+			})
+			
+			
+			$('#showrefFrankenstein').click(function() {
+				if (this.checked) 
+					$('q.refFrankenstein').addClass('text-refFrankenstein')
+				else
+					$('q.refFrankenstein').removeClass('text-refFrankenstein')
+			})
+		}
+		
+		function load(file) {
+			$.ajax({
+				method: 'GET',
+				url: file,
+				success: function(d) {
+					$('#file').html(d)
+					$('.show').prop("checked", false)
+					addIds()
+					filltabs()
+					$('#title').html($('#file h1'))
+				},
+				error: function() {
+					alert('Could not load file '+file)
+				}
+			});
+		}
+		
+		function addIds() {
+			addId('.person','person')
+			addId('.character', 'character')
+			addId('.literaryWork', 'literaryWork')
+		}
+		
+		function addId(what, prefix) {
+			var id = '0'
+			var elements = $(what); 
+			for (var i=0; i<elements.length; i++) {
+				elements[i].id = prefix + "-" + id++
+			}
+		}
+		function filltabs(){
+			fillInfo("#file", "#info")
+			filltab("#file .person","list-person","#person")
+			filltab("#file .character","list-character","#character")
+			filltab("#file .literaryWork","list-literaryWork","#literaryWork")
+		}
+		
+		function fillInfo(from, where) {
+			var item = `
+				<p class="list title"><b>Title: </b> $title</p>
+				<p class="list author"><b>Author: </b> $author</p>
+				<p class="list author"><b>Publishing date: </b> $date</p>
+				<p class="list author"><b>Issue: </b> $issue</p>
+				<p class="list author"><b>Headings: </b><ul>$headings</ul></p>
+				
+				` ;
+			$(where).empty(); 
+
+			var title = $(from + ' h1')[0].innerText
+			var author = $(from + ' .byline')[0].innerText
+		    var date = $(from + ' .dateline')[0].innerText
+			var issue = $(from + ' .issued')[0].innerText
+			var headingList = $(from + ' h2')
+			var headings = ""
+			for (var i=0; i<headingList.length; i++) {
+				headings += "<li class='small'>"+headingList[i].innerHTML+"</li>"
+			}
+			$(where).append(item.tpl( {
+				author: author,
+				title: title,
+				date: date,
+				issue: issue,
+				headings: headings
+			}))
+		}
+		
+		function filltab(what,style,where) {
+			var list = `<li class="list $style"><a href="#" onclick="goto('$place')">$content</a></li>`
+			var elements = $(what); 
+			$(where+' ul').empty(); 
+			for (var i=0; i<elements.length; i++) {
+				$(where+' ul').append(list.tpl({
+					style:style, 
+					place: '#'+elements[i].id,
+					content: elements[i].innerHTML
+				}) )
+			}
+		}
+
+		function goto(id) {
+			var t = $(id)[0].offsetTop;
+			$('body').animate({ scrollTop: t }, 200);
+			$(id).addClass('animate');
+			setTimeout(function(){
+				$(id).removeClass('animate');
+			},5000);
+		}
+		
