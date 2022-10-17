58488b6e05c4ff3268e7c4cc4e4bbe7c3291de1d
Nora <norapasquale@gmail.com>
2022-10-17 22:32:55 +0200

    sticky 3

--- Scripts/mainjs.js
+++ Scripts/mainjs.js
@@ -204,3 +204,12 @@ function goTop() {
 		
 		
 		
+let parent = document.querySelector('.metadata').parentElement;
+
+while (parent) {
+    const hasOverflow = getComputedStyle(parent).overflow;
+    if (hasOverflow !== 'visible') {
+        console.log(hasOverflow, parent);
+    }
+    parent = parent.parentElement;
+}
