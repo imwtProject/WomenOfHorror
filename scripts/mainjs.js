3ab00d907a3c73c636b8dd13a092b6648c8a5d05
MaddaGh <92437363+MaddaGh@users.noreply.github.com>
2022-12-08 18:04:18 +0100

    Update mainjs.js

--- scripts/mainjs.js
+++ scripts/mainjs.js
@@ -294,8 +294,7 @@ function goTop() {
 		function goto(id) {
 			var t = $(id)[0].offsetTop; /* DOM element, read only property*/
 			var x = $(id).offset()
-			console.log(x.top)
-			 $('html, body').animate(
+			$('#main-article').animate(
 				{ /*performs a custom animation of a set of CSS properties. (selector).animate({styles},speed,easing,callback)*/
         	scrollTop: x.top - 130} /* jQuery method */
 												, 200); 
@@ -305,9 +304,7 @@ function goTop() {
 			setTimeout(function(){
 				$(id).removeClass('animate');
 			},5000);
-
-			alert("Top: " + x.top + " Left: " + x.left);
-		}
+			}
 		
 /* fab styles */ 
 function fabopener()	{
